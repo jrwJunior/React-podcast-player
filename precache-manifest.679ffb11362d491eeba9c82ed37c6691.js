607008a3789b49fd56e6c7a40027811a276157a0
Robert <runshacking@gmail.com>
2019-10-25 13:55:28 +0300

    Updates

---
+++ precache-manifest.679ffb11362d491eeba9c82ed37c6691.js
@@ -0,0 +1,28 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50da74e0db9145366cdbd9ffab94553d",
+    "url": "/podcast-player/index.html"
+  },
+  {
+    "revision": "8cf6f4c9f704d903a62d",
+    "url": "/podcast-player/static/css/main.1b973732.chunk.css"
+  },
+  {
+    "revision": "03ab5c4058ee452a5268",
+    "url": "/podcast-player/static/js/2.a37fbcec.chunk.js"
+  },
+  {
+    "revision": "8cf6f4c9f704d903a62d",
+    "url": "/podcast-player/static/js/main.b480da88.chunk.js"
+  },
+  {
+    "revision": "5798c3a1c2168ab365bb",
+    "url": "/podcast-player/static/js/runtime-main.18ffd589.js"
+  },
+  {
+    "url": "/podcast-player/static/media/dark.c610b4800820071e9b19f0542110499c.c610b480.gif"
+  },
+  {
+    "url": "/podcast-player/static/media/inverse.ed90d7bca6550746a2a2c5d538d62efe.ed90d7bc.gif"
+  }
+]);
